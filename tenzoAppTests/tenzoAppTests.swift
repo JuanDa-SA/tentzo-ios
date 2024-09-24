06e7a8603eaf35a80e18676bd5cff39634172c22
Javier <javierch140504@outlook.com>
2024-09-24 13:13:41 -0600

    Initial Commit

---
+++ tenzoAppTests/tenzoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  tenzoAppTests.swift
+//  tenzoAppTests
+//
+//  Created by Javier Cuatepotzo on 24/09/24.
+//
+
+import Testing
+@testable import tenzoApp
+
+struct tenzoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
